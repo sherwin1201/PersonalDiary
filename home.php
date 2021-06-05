8d15ad5941951f034896afb9d6cefc9b76309538
Sherwin Fernandes <54794569+sherwin1201@users.noreply.github.com>
2021-06-05 20:35:31 +0530

    Added edit option and a few prompts

--- home.php
+++ home.php
@@ -35,18 +35,48 @@
     </div>
     <div> <?php include('errors.php'); ?> </div>
     <form action="home.php" method="post">
+        
         <label>Personal</label></br>
-        <textarea required name="personaldiary"><?php if(isset($_SESSION['username'])) displaydata($_SESSION['username'],'personaldiary') ?></textarea></br>
+        <textarea required name="personaldiary"><?php if(isset($_SESSION['username'])) 
+            displaydata($_SESSION['username'],'personaldiary') ?></textarea></br>
+        
         <label>Work</label></br>
         <textarea required name="workdiary"><?php if(isset($_SESSION['username'])) displaydata($_SESSION['username'],'workdiary') ?></textarea></br>
+        
         <button type="submit" name="submitdiary" id="submit-btn" 
         <?php 
             if(isset($_SESSION['username']))
                 if(submitcheck($_SESSION['username'])) : ?>
                 hidden <?php endif ?>>Submit</button>
+        
+        <?php 
+            if(isset($_SESSION['username']))
+                if(submitcheck($_SESSION['username'])) : ?>
+                 <button type="submit" name="editdiary" id="edit-btn">Edit</button>
+            <?php endif ?>
     </form>
     <div>
-        <p> <a href="home.php?logout='1">logout</a> </p>
+        <p> <a href="home.php?logout='1" id="logout">logout</a> </p>
     </div>
 </body>
 </html>
+
+<script>
+    $("#edit-btn").click(function(event){
+        if(!confirm('Are you sure you want to confirm your changes?')){
+            event.preventDefault();
+        }
+    });
+
+    $("#submit-btn").click(function(event){
+        if(!confirm('Are you sure you want to submit your diary?')){
+            event.preventDefault();
+        }
+    });
+
+    $("#logout").click(function(event){
+        if(!confirm('You will be logged out of the portal.\nDo you want to continue?')){
+            event.preventDefault();
+        }
+    });
+</script>
